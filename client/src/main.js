5af7a8fd2393b83f3c3ffdab356e3a6455d1ecb9
Aurélien <aureliendp@free.fr>
2022-03-16 11:37:53 -0400

    [Refactor] Added client directory

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router/router.js'
 
 import 'bootstrap'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App)
     .use(router)
